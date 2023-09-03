0033e282c9f28cb3eec41b5e2bbbd2e3598a13e0
Yoonmen <sgu101310@naver.com>
2023-09-03 21:26:43 +0900

    mainUI - 아이디 저장 ckb 추가

--- PEFE_mainUI.py
+++ PEFE_mainUI.py
@@ -1,10 +1,10 @@
 from img.img import *
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton
+from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QCheckBox, QMessageBox
 from PySide2.QtCore import Qt
-from PySide2.QtGui import QIcon, QFontDatabase, QCursor
+from PySide2.QtGui import QIcon, QFontDatabase, QCursor, QFont
 
-from os import path as osPath
+from os import path as osPath, environ
 from enum import Enum
 
 
@@ -29,6 +29,26 @@ class StyleSheets(Enum) :
                         "font-weight : bold;\n"
                     "}")
     
+    check_box = ("QCheckBox{\n"
+                    "color : #a6a6a6;\n"
+                "}\n"
+                "QCheckBox::indicator::unchecked{\n"
+                    "image : url(:/img/ckb_unchecked_normal.png);\n"
+                    "width : 20px;\n"
+                    "height : 20px;\n"
+                "}\n"
+                "QCheckBox::indicator::unchecked::hover{\n"
+                    "image : url(:/img/ckb_unchecked_hover.png);\n"
+                "}\n"
+                "QCheckBox::indicator::checked{\n"
+                    "image : url(:/img/ckb_checked_normal.png);\n"
+                    "width : 20px;\n"
+                    "height : 20px;\n"
+                "}\n"
+                "QCheckBox::indicator::checked::hover{\n"
+                    "image : url(:/img/ckb_checked_hover.png);\n"
+                "}")
+    
     action_button = ("QPushButton{\n"
                         "background-color : transparent;\n"
                         "font-family : 나눔고딕OTF;\n"
@@ -48,7 +68,6 @@ class MainUI(QMainWindow) :
 
         self.mainUI()
 
-        self.signal()
 
     def mainUI(self) : 
         # basic_part
@@ -87,11 +106,36 @@ class MainUI(QMainWindow) :
 
 
         # action_part
-        self.start_bt = QPushButton(self)
-        self.start_bt.setGeometry(123, 245, 412, 46)
-        self.start_bt.setStyleSheet(StyleSheets.start_button.value)
-        self.start_bt.setText("삭제 시작")
-        self.start_bt.setCursor(QCursor(Qt.PointingHandCursor))
+        self.login_bt = QPushButton(self)
+        self.login_bt.setGeometry(123, 245, 412, 46)
+        self.login_bt.setStyleSheet(StyleSheets.start_button.value)
+        self.login_bt.setText("로그인 및 데이터 가져오기")
+        self.login_bt.setCursor(QCursor(Qt.PointingHandCursor))
+        self.login_bt.setFocusPolicy(Qt.NoFocus)
+
+        self.startDelete_bt = QPushButton(self)
+        self.startDelete_bt.setGeometry(123, 245, 412, 46)
+        self.startDelete_bt.setStyleSheet(StyleSheets.start_button.value)
+        self.startDelete_bt.setText("삭제 시작")
+        self.startDelete_bt.setCursor(QCursor(Qt.PointingHandCursor))
+        self.startDelete_bt.setFocusPolicy(Qt.NoFocus)
+        self.startDelete_bt.hide()
+
+        self.remember_ckb = QCheckBox(self)
+        self.remember_ckb.setGeometry(137, 308, 107, 18)
+        self.remember_ckb.setFont(QFont("나눔고딕OTF", 12))
+        self.remember_ckb.setStyleSheet(StyleSheets.check_box.value)
+        self.remember_ckb.setFocusPolicy(Qt.NoFocus)
+        self.remember_ckb.setText("아이디 기억")
+
+        appdata_path = osPath.join(environ["LOCALAPPDATA"], "PEFE")
+        appdata_file = osPath.join(appdata_path, "data.dat")
+        if osPath.isfile(appdata_file) : 
+            self.remember_ckb.setChecked(True)
+            with open(appdata_file, 'r') as file : 
+                user_id = file.read()
+                print(file.read())
+            self.userId_le.setText(user_id)
 
         self.forgot_bt = QPushButton(self)
         self.forgot_bt.setGeometry(378, 307, 150, 20)
@@ -101,6 +145,7 @@ class MainUI(QMainWindow) :
                                         "}")
         self.forgot_bt.setText("아이디/비밀번호 찾기")
         self.forgot_bt.setCursor(QCursor(Qt.PointingHandCursor))
+        self.forgot_bt.setFocusPolicy(Qt.NoFocus)
 
         self.checkPosting_lb = QLabel(self)
         self.checkPosting_lb.setGeometry(178, 357, 264, 18)
@@ -117,11 +162,30 @@ class MainUI(QMainWindow) :
                                             "}")
         self.checkPosting_bt.setText("확인")
         self.checkPosting_bt.setCursor(QCursor(Qt.PointingHandCursor))
+        self.checkPosting_bt.setFocusPolicy(Qt.NoFocus)
 
 
+        # messageBox_part
+        self.userAccountNull_msgBox = QMessageBox()
+        self.userAccountNull_msgBox.setWindowTitle("Account Error")
+        self.userAccountNull_msgBox.setText("아이디와 비밀번호를 모두 입력해 주세요.")
+        self.userAccountNull_msgBox.setIcon(QMessageBox.Critical)
+        if osPath.isfile(icon_path) : 
+            self.userAccountNull_msgBox.setWindowIcon(QIcon(icon_path))
 
-    def signal(self) : 
-        pass                # Test code / please delete the contents of this line.
+        self.notLogin_msgbox = QMessageBox()
+        self.notLogin_msgbox.setWindowTitle("Data Error")
+        self.notLogin_msgbox.setText("로그인 및 데이터 가져오기 후에 다시 시도해 주십시오.")
+        self.notLogin_msgbox.setIcon(QMessageBox.Critical)
+        if osPath.isfile(icon_path) : 
+            self.notLogin_msgbox.setWindowIcon(QIcon(icon_path))
+        
+        self.solvePuzzle_msgBox = QMessageBox()
+        self.solvePuzzle_msgBox.setWindowTitle("사용자 퀘스트 발행")
+        self.solvePuzzle_msgBox.setText("캡차 퍼즐 해결 후 로그인 버튼 클릭까지 진행해 주십시오.")
+        self.solvePuzzle_msgBox.setIcon(QMessageBox.Warning)
+        if osPath.isfile(icon_path) : 
+            self.solvePuzzle_msgBox.setWindowIcon(QIcon(icon_path))
 
 
 
